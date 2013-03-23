278a420367ac2291594abd9a801843b092edecc5
Damian C. Williams <damian@askagp.com>
2013-03-23 23:01:00 +0000

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doctordamian::Application.config.secret_token = '1b9c8b43606492c0b9986d66372be0a2fa8f4de546f90e1af05d51ac0988158742648ee525accbc09ae9ed1c6f2f0c36f128b803b5d325bdd5fa6b3bb051482c'
